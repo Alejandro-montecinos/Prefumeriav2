7515f3241c0c14df2b85903597769012b8e70fe6
Alejandro-montecinos <alejandromontecino002@gmail.com>
2025-05-13 12:58:55 -0400

    se agrega el proyecto

---
+++ Perfumeriav2/Perfumeriav2/src/main/java/com/examplePerfumeria/Perfumeriav2/Perfumeriav2Application.java
@@ -0,0 +1,13 @@
+package com.examplePerfumeria.Perfumeriav2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Perfumeriav2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Perfumeriav2Application.class, args);
+	}
+
+}
